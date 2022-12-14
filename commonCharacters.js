6c4f17b7430ad7636ae0c28a650311536fd87db1
chiraz711 <chiraz26mechi@gmail.com>
2022-12-14 14:48:03 +0100

    solution of problem

--- commonCharacters.js
+++ commonCharacters.js
@@ -12,8 +12,19 @@
 
 
 
- var commonCharacters = function(string1, string2) {
-  
-    }
+var commonCharacters = function(string1, string2) {
+   var result='';
+   if (string1.length === 0 || string2.length === 0) {
+      result = "";
+   } else {
+   for (var i = 0; i< string1.length; i++) {
+   for(var j=0;j<string2.length;j++){
+      if (string1[i] === string2[j] && !result.includes(string2[j])) {
+         result=result+string2[j];
+   }
+   }
+   }
     
-    
+   }
+   return result; 
+   }
